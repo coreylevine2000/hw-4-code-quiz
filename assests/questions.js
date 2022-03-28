da59840bbe760823cb09d80f69bed2dabbd7084d
Corey Levine <coreylevine2000@gmail.com>
2022-03-27 21:59:49 -0400

    Questions finalized, fixing known errors

--- assests/questions.js
+++ assests/questions.js
@@ -4,60 +4,60 @@ let questions = [
         numb: 1,
         question: "How long was the 100 Years War between England and France?",
         options: ["98 years", "100 years", "149 years", "114 years"],
-        answer: 3
+        answer: "114 years"
     },
     {
         numb: 2,
         question: "The American War for Independence began in 1776, but when did America offically win the war?",
         options: ["1776", "1781", "1783", "1789"],
-        answer: 2
+        answer: "1783"
     },    
     {
         numb: 3,
         question: "Which war was also know as The Great War",
         options: ["World War 1", "World War 2", "The American Civil War", "The Cold War"],
-        answer: 0
+        answer: "World War 1"
     },    
     {
         numb: 4,
         question: "When Christopher Columbus discovered the New World, the three ships were named the Nina, the Pinta, and the ...",
         options: ["Santa Monica", "Santa Maria", "Forida", "Sangria"],
-        answer: 1
+        answer: "Santa Maria"
     },    
     {
         numb: 5,
         question: "The Library of Alexander was located in which ancint civilization?",
         options: ["Rome", "Egypt", "Athens", "Sparta"],
-        answer: 1
+        answer: "Egypt"
     },    
     {
         numb: 6,
         question: "Which Historical Empire is known to be the biggest in size?",
         options: ["Mongol", "Russian", "Qing", "British"],
-        answer: 3
+        answer: "British"
     },    
     {
         numb: 7,
         question: "Which country was origially named Siam",
         options: ["Mongol", "Cambodia", "Laos", "Thialand"],
-        answer: 3
+        answer: "Thialand"
     },    
     {
         numb: 8,
         question: "Which country was origially named Ceylon",
         options: ["Bengal", "Cambodia", "Sri Lanka", "Thialand"],
-        answer: 2
+        answer: "Sri Lanka"
     },    
     {
         numb: 9,
         question: "Who were the main economic rivals during the Cold War",
         options: ["Russia and China", "United States and China", "Norway and United States", "Russia and United States"],
-        answer: 3
+        answer: "Russia and United States"
     },    
     {
         numb: 10,
         question: "Which ancient civilization was located between the rivers Tigris and Euphrates?",
         options: ["Indus Valley", "Mesopotamia", "Incan", "Mayan"],
-        answer: 1
+        answer: "Mesopotamia"
     },
 ];
